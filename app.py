8f1d922e2943fef40767069e774743d278a38d49
RosTHunT <98609332+RosTHunT@users.noreply.github.com>
2022-03-31 13:14:50 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -38,9 +38,67 @@ def about():
 
 
 
-@app.route('/create-article')
+@app.route('/posts')
+def posts():
+	articles = Article.query.order_by(Article.date.desc()).all()
+	return render_template("posts.html", articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+	article = Article.query.get(id)
+	return render_template("post_detail.html", article=article)
+
+
+@app.route('/posts/<int:id>/del')
+def post_delete(id):
+	article = Article.query.get_or_404(id)
+
+	try:
+		db.session.delete(article)
+		db.session.commit()
+		return redirect('/posts')
+	except:
+		return "при удаление статьи произошла ошибка"
+
+
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def post_update(id):
+	article = Article.query.get(id)
+	if request.method == "POST":
+		article.title = request.form['title']
+		article.intro = request.form['intro']
+		article.text = request.form['text']
+
+
+		try:
+	
+			db.session.commit()
+			return redirect('/posts')
+		except:
+			return "при редактирование статьи произошла ошибка"
+	else:
+				return render_template("post_update.html", article=article)
+
+
+@app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
-	return render_template("create-article.html")
+	if request.method == "POST":
+		title = request.form['title']
+		intro = request.form['intro']
+		text = request.form['text']
+
+
+		article = Article(title=title, intro=intro, text=text) 
+
+		try:
+			db.session.add(article)
+			db.session.commit()
+			return redirect('/posts')
+		except:
+			return "при добавлении статьи произошла ошибка"
+	else:
+		return render_template("create-article.html")
 
 
 
